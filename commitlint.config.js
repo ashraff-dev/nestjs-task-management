780a8dc8ad12d43d4b17d63f99a3e4404e8858ef
ashraffdev <ashraf@rapidinnovation.dev>
2022-11-17 13:39:09 +0530

    PH-16: Testing husky

---
+++ commitlint.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    plugins: ['commitlint-plugin-jira-rules'],
+    extends: ['jira'],
+ 
+  }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,22 +1,22 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersRepository } from './users.repository';
-import { PassportModule } from '@nestjs/passport';
-import { JwtModule } from '@nestjs/jwt';
-import { JwtStrategy } from './jwt.strategy';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { PassportModule } from "@nestjs/passport";
+import { JwtModule } from "@nestjs/jwt";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { AuthService } from "./auth.service";
+import { AuthController } from "./auth.controller";
+import { UsersRepository } from "./users.repository";
+import { JwtStrategy } from "./jwt.strategy";
 
 @Module({
   imports: [
     ConfigModule,
-    PassportModule.register({ defaultStrategy: 'jwt' }),
+    PassportModule.register({ defaultStrategy: "jwt" }),
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
-        secret: configService.get('JWT_SECRET'),
+        secret: configService.get("JWT_SECRET"),
         signOptions: {
           expiresIn: 3600,
         },
